f2320fdc762077f67f4f633c6093ac35860644cd
Vu Dinh <isthis.vu@gmail.com>
2016-08-24 16:33:18 -0700

    added server folder

---
+++ server/config/mongoose.js
@@ -0,0 +1,16 @@
+console.log("IN SERVER/CONFIG/MONGOOSE.JS FILE")
+
+var mongoose = require("mongoose");
+
+var fs = require("fs");
+
+mongoose.connect("mongodb://localhost/macadons");
+
+var models_path = __dirname + "/../models"
+
+
+fs.readdirSync(models_path).forEach(function(file){
+	if(file.indexOf(".js") > 0) {
+		require(models_path + "/" + file);
+	};
+})
---
+++ server/config/routes.js
@@ -0,0 +1,8 @@
+console.log("IN SERVER/CONFIG/ROUTES.JS FILE");
+
+// var users = require("../controllers/users.js");
+
+module.exports = function(app){
+	
+
+}
